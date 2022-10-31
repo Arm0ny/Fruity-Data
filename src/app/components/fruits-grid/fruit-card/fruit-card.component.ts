3ce66fdc36e3a1f49b807ae9c77a9ced10322c7f
Arm0ny <63424081+Arm0ny@users.noreply.github.com>
2022-10-31 10:29:03 +0100

    from grid to flex

---
+++ src/app/components/fruits-grid/fruit-card/fruit-card.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FruitInterface} from "../../../interfaces/FruityInterface";
+import {addDoc} from "@angular/fire/firestore";
+
+@Component({
+  selector: 'app-fruit-card',
+  templateUrl: './fruit-card.component.html',
+  styleUrls: ['./fruit-card.component.css']
+})
+export class FruitCardComponent implements OnInit {
+  @Input() fruit?: FruitInterface
+
+  showDetails: Boolean = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onClick($event : any){
+    this.showDetails = !this.showDetails
+    let fruitCard : HTMLElement = $event.target
+    fruitCard.parentElement?.classList.add('col-span-2')
+  }
+
+}
